be443554e325b94791639ea1bebe99710103605f
Lucas Palharini <lucas230991@hotmail.com>
2024-11-03 14:57:45 -0300

    🚨 fix: enhance error handling with detailed user response

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,8 +1,12 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { HttpError } from '../errors/HttpError';
 
-export function errorHandler(err: HttpError, _req: Request, res: Response): void {
-    console.error('Erro:', err);
-
-    res.status(err.statusCode).send({ error: err.message });
+/* eslint-disable @typescript-eslint/no-unused-vars */
+export function errorHandler(err: HttpError, _req: Request, res: Response, _next: NextFunction): void {
+    console.error(`Erro ${err.statusCode}: ${err.message}`);
+    res.status(err.statusCode).json({
+        status: 'error',
+        statusCode: err.statusCode,
+        message: err.message,
+    });
 }
