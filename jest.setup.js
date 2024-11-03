6c1d34a1d350f5f1e2351639530ac379a0483c14
Lucas Palharini <lucas230991@hotmail.com>
2024-11-03 18:43:33 -0300

    🔧 chore: add Jest scripts and configuration files with MongoDB in-memory server

---
+++ jest.setup.js
@@ -0,0 +1,16 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+
+let mongoServer;
+
+beforeAll(async () => {
+    mongoServer = await MongoMemoryServer.create();
+    const uri = mongoServer.getUri();
+
+    await mongoose.connect(uri);
+});
+
+afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoServer.stop();
+});
