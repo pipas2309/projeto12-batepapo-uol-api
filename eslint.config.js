6646558031720ac98d65175b49a7149b2ccc64d6
Lucas Palharini <lucas230991@hotmail.com>
2024-11-03 14:00:29 -0300

    🛠️ refactor: migrate .eslintignore to eslint.config.js and update ignore settings

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,16 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.node }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+    {
+        files: ['**/*.{js,mjs,cjs,ts}'],
+        ignores: ['node_modules', 'dist'],
+        languageOptions: {
+            globals: globals.node,
+        },
+    },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
 ];
