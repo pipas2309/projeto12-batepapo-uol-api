167b8d98cd46685c9273ab63ea93599234ff4e6c
Lucas Palharini <lucas230991@hotmail.com>
2024-11-03 10:47:21 -0300

    🔧 chore: add ESLint, Prettier, EditorConfig configuration for consistent coding styles

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module', // import/export
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
+  rules: {
+    // Regras personalizadas aqui
+    'prettier/prettier': 'error',
+    'import/no-unresolved': 'error',
+    'no-console': 'warn',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+};
