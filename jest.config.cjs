46c544437c2f29008e700295f7ff231da31271aa
Lucas Palharini <lucas230991@hotmail.com>
2024-11-04 20:45:01 -0300

    🔧 chore: update Jest configuration and change extension to .cjs

---
+++ jest.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    setupFiles: ['dotenv/config'],
+    clearMocks: true,
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    collectCoverageFrom: [
+        'src/**/*.{ts,tsx}',
+        '!src/index.ts',
+        '!src/config/swagger.ts',
+        '!src/config/database.ts',
+    ],
+    setupFilesAfterEnv: ['<rootDir>/tests/setupTests.ts'],
+};
