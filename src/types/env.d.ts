732519ee72df0934bf5003733396abd37ba24a8c
Lucas Palharini <lucas230991@hotmail.com>
2024-11-04 20:44:10 -0300

    📝 docs: add TypeScript definitions for environment variables

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,7 +1,9 @@
 declare namespace NodeJS {
     interface ProcessEnv {
         PORT?: string;
+        NODE_ENV: string;
         URL_CONNECT_MONGO: string;
+        URL_CONNECT_MONGO_TEST: string;
         LOGOUT_TIME: string;
         ACTIVITY_CHECKER_TIME: string;
     }
