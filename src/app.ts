164364edd678248bd2a6c8d1c3dd8503e8fe905f
Lucas Palharini <lucas230991@hotmail.com>
2024-11-07 02:04:39 -0300

    ✨ feat: integrate Morgan middleware for HTTP request logging

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,15 @@ import cors from 'cors';
 import router from './routes';
 import { errorHandler } from './middlewares/errorHandler';
 import { swaggerUi, swaggerSpec } from './config/swagger';
+import morganMiddleware from '@infra/config/morganMiddleware';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Morgan para registrar requisições HTTP
+app.use(morganMiddleware);
 
 // Rota para a documentação do Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
